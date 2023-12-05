f65db530f92015e2e4a8f97937c27c4f9e8f9e86
nakano79 <ashw0709@naver.com>
2023-12-05 18:14:09 +0900

    add model select:   test2.py

--- test2.py
+++ test2.py
@@ -1,12 +1,7 @@
 import g4f
-
-# g4f.debug.logging = False  # Enable logging
-# g4f.check_version = False  # Disable automatic version checking
-# print(g4f.version)  # Check version
-# print(g4f.Provider.Ails.params)  # Supported args
-
 from g4f.Provider import (
-    AItianhu,
+    AItianhu,           
+    AItianhuSpace,
     Aichat,
     Bard,
     Bing,
@@ -19,17 +14,28 @@ from g4f.Provider import (
     Raycast,
     Liaobots,
     GeekGpt,
-    Phind
+    Phind,
+    FreeGpt
 )
 
+print("Now Starting...")
+print("which model do you want? \n1) gpt-3.5 \n2) gpt-4")
+modsel = int(input())
+if modsel == 1:
+    model="gpt-3.5-turbo"
+    provider=g4f.Provider.GeekGpt
+else :
+    model="gpt-4"   
+    provider=g4f.Provider.Bing
+print("initialize complete!")
 conversation_history = []
 
 while True:
     user_input = str(input())
     messages = conversation_history + [{"role": "user", "content": user_input}]
     response = g4f.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        provider=g4f.Provider.You,
+        model=model,
+        provider=provider,
         messages=messages
     )
 
